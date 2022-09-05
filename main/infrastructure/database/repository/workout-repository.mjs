57c8401c54b66b922c160add37156a19f3949f80
Vitalii Honchar <honchar.v@ajax.systems>
2022-09-05 14:32:32 -0400

    Added convert workout id to string

--- main/infrastructure/database/repository/workout-repository.mjs
+++ main/infrastructure/database/repository/workout-repository.mjs
@@ -28,7 +28,7 @@ export default class WorkoutRepository {
             TableName: TABLE_WORKOUT,
             Item: {
                 Coach: {S: workout.coach},
-                Id: {N: workout.id},
+                Id: {N: `${workout.id}`},
                 Name: {S: workout.name},
                 Description: {S: workout.description},
                 Videos: {SS: workout.videos.map(v => v.link)}
